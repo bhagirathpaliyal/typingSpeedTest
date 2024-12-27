e9682b0b6522e867453aacc053386cc4b91fb4e3
bhagirathpaliyal <bhagirathpaliyal@gmail.com>
2024-12-27 11:28:12 +0530

    added DarkMode support

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,31 @@
-const Header = () => (
-    <h1 className="text-3xl text-[#B0B0B0] font-bold mb-4 animate-fadeIn">
-      Typing Speed Test
-    </h1>
-  );
+import { useState } from "react";
+
+const Header = () => {
+
+ const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.documentElement.classList.toggle('dark', !darkMode);
+  };
+    return(
+    <div className=" flex justify-between w-[90%]">
+      <h1 className="text-3xl text-[#B0B0B0]  font-bold mb-4 animate-fadeIn">Typing Speed Test</h1>
+
+      <button
+          onClick={toggleDarkMode}
+          className="  px-5 py-2 rounded-lg 
+          bg-gradient-to-r from-[#FFFFFF] via-[#F5F5F5] to-[#D6D6D6]
+          dark:from-[#1E1E1E] dark:via-[#2E2E2E] dark:to-[#3E3E3E]
+          shadow-inner
+          text-gray-800 dark:text-gray-200
+          hover:shadow-md
+          transition-all"
+        >
+          {darkMode ? 'Light Mode' : 'Dark Mode'}
+        </button>
+    </div>
+)};
   
   export default Header;
   
--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -8,18 +8,18 @@ interface StatsProps {
   }
 
 const Stats : React.FC<StatsProps> = ({ wordsTyped, correctChars, accuracy, timeLeft }) => (
-    <div className="flex flex-wrap-reverse gap-[2px] justify-center mt-6 w-full md:w-2/3 space-y-2 md:space-y-0">
-      <div className="w-full md:w-1/4 flex justify-center items-center p-2 border rounded-lg">
-        Words Typed: {wordsTyped}
+    <div className="flex flex-wrap-reverse gap-[2px] justify-center mt-6 w-full md:w-2/3 space-y-2 md:space-y-0 dark:text-[#fff] text-[#000]">
+      <div className="w-full md:w-1/4 flex justify-center items-center p-2 border-2 dark:border rounded-lg">
+        WPM: {wordsTyped}
       </div>
-      <div className="w-full md:w-1/4 flex justify-center items-center p-2 border rounded-lg">
-        Correct Characters: {correctChars}
+      <div className="w-full md:w-1/4 flex justify-center items-center p-2 border-2 dark:border rounded-lg">
+        Correct Char: {correctChars}
       </div>
-      <div className="w-full md:w-1/4 flex justify-center items-center p-2 border rounded-lg">
-        Accuracy: {accuracy}%
+      <div className="w-full md:w-1/4 flex justify-center items-center p-2 border-2 dark:border rounded-lg">
+        Acc: {accuracy}%
       </div>
-      <div className="w-full md:w-1/4 flex justify-center items-center p-2 border rounded-lg">
-        Time Left: {timeLeft}s
+      <div className="w-full md:w-1/4 flex justify-center items-center p-2 border-2 dark:border rounded-lg">
+        Time: {timeLeft}s
       </div>
     </div>
   );
