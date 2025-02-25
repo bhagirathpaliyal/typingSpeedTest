f7e55315b7ed91445431152d6463f5fd5616f49f
bhagirathpaliyal <bhagirathpaliyal@gmail.com>
2025-02-25 18:33:38 +0530

    update:deploy.yml

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -10,10 +10,11 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
 };
-console.log("API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
+
 
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
 
 export { db, collection, addDoc, getDocs, query, orderBy };
